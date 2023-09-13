a71882749d6e712466039c0defb49c3ec99857bb
YusufGulcan <yusuf.gulcan0@gmail.com>
2023-09-13 12:55:52 +0300

    replace

---
+++ World_economy.py
@@ -0,0 +1,16 @@
+import plotly.express as px
+import streamlit as st
+import pandas as pd
+import numpy as np 
+import plotly.express as px
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+
+
+#path = 'C:\Users\yusuf\OneDrive\Documents\Data Science Projects\World Economy\World_economy.csv'
+#df = pd.read_csv(path)
+df = 3+5
+print(df)
+
+
